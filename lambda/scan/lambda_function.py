7de179d39cf4c5903f9000301ed4f3c1a3165c8a
Jeff Thompson <jefft0@remap.ucla.edu>
2019-08-27 14:59:36 -0700

    scan lambda_funcion.py: Finish the logic to check for exiting another experience.

--- lambda/scan/lambda_function.py
+++ lambda/scan/lambda_function.py
@@ -23,13 +23,13 @@ def lambda_handler(event, context):
         aws_session_token=SESSION_TOKEN
     )
 
-    # Get all events with the visitor_id and experience_id.
     visitorEventTable = dynamodb.Table('fssi2019-dynamodb-visitor_event_ts')
+    occupancyTable = dynamodb.Table('fssi2019-dynamodb-occupancy')
+
+    # Get all events with the visitor_id.
     # Note: Scan reads all records from DynamoDB. Maybe set up a secondary index.
     visitorEvents = visitorEventTable.scan(
-        FilterExpression=Attr('visitor_id').eq(visitor_id) &
-                         Attr('experience_id').eq(experience_id)
-    )
+      FilterExpression=Attr('visitor_id').eq(visitor_id))
 
     # Get the visitor event with the latest timestamp.
     latestEvent = None
@@ -40,23 +40,51 @@ def lambda_handler(event, context):
     print("Debug: latestEvent: " + str(latestEvent))
     
     # Figure out whether the event means that the visitor is entering the experience.
-    if latestEvent == None or latestEvent['event'] == 'exit':
+    if latestEvent == None:
         isEntering = True
+    elif latestEvent['experience_id'] == experience_id:
+        if latestEvent['event'] == 'exit':
+            # Re-entering this experience.
+            isEntering = True
+        else:
+            # Now exiting.
+            isEntering = False
     else:
-        isEntering = False
+        # The latest event was for a different experience. We have to be
+        # entering this experience.
+        isEntering = True
+        if latestEvent['event'] != 'exit':
+            # The visitor must have exited the other experience without scanning.
+            # Add an exit event for the other experience and remove from occupancy.
+            visitorEventTable.put_item(Item=
+              { 'timestamp': Decimal(time.time()),
+                'visitor_id': visitor_id,
+                'experience_id': latestEvent['experience_id'],
+                'event': 'exit',
+                'confidence': Decimal(1.0) })
 
-    # TODO: If latestEvent['event'] == 'off-board', do we need to on-board before entry?
+            occupancyResponse = occupancyTable.get_item(Key=
+              { 'id': latestEvent['experience_id'] })
+            if 'Item' in occupancyResponse:
+                occupancy = occupancyResponse['Item']['occupancy']
+                occupancy.discard(visitor_id)
+                if len(occupancy) == 0:
+                    # The empty set is not allowed, so remove the entry.
+                    occupancyTable.delete_item(Key=
+                      { 'id': latestEvent['experience_id'] })
+                else:
+                    occupancyTable.put_item(Item=
+                      { 'id': latestEvent['experience_id'], 'occupancy': occupancy })
 
     # Add a new visitor event.
     visitorEventTable.put_item(Item=
-      { 'timestamp': time.time(),
+      { 'timestamp': Decimal(time.time()),
         'visitor_id': visitor_id,
         'experience_id': experience_id,
         'event': 'entry' if isEntering else 'exit',
-        'confidence': Decimal('1.0') })
+        'confidence': Decimal(1.0) })
 
     # Update the occupancy table for the experience based on isEntering.
-    occupancyTable = dynamodb.Table('fssi2019-dynamodb-occupancy')
     occupancyResponse = occupancyTable.get_item(Key={ 'id': experience_id })
     if 'Item' in occupancyResponse:
         occupancy = occupancyResponse['Item']['occupancy']
@@ -65,8 +93,12 @@ def lambda_handler(event, context):
         else:
             occupancy.discard(visitor_id)
 
-        occupancyTable.put_item(Item=
-          { 'id': experience_id, 'occupancy': occupancy })
+        if len(occupancy) == 0:
+            # The empty set is not allowed, so remove the entry.
+            occupancyTable.delete_item(Key={ 'id': experience_id })
+        else:
+            occupancyTable.put_item(Item=
+              { 'id': experience_id, 'occupancy': occupancy })
     else:
         # The is no entry for the experience_id. Create it if is entering.
         if isEntering:
