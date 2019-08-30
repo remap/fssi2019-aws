8e79285fb211436b0c43548c506592d4984479f4
peetonn <gpeetonn@gmail.com>
2019-08-30 11:49:11 -0700

    fixed get module paths script

--- touch/get-module-path.sh
+++ touch/get-module-path.sh
@@ -2,7 +2,6 @@
 CWD="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
 ENV=$CWD/../env
 TOUCH=$HOME
-PYMODULES=`ls $ENV/lib/python3.7/site-packages/`
-
-export PYTHONPATH=$PYTHONPATH:$PYMODULES
+# PYMODULES=`ls $ENV/lib/python3.7/site-packages/`
+# export PYTHONPATH=$PYTHONPATH:$PYMODULES
 echo -e "import sys\nfor p in sys.path:  print(p)" | python > $TOUCH/sys-paths.txt
