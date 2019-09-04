a19c704459d699765f381076d39d96ca77b3b488
peetonn <gpeetonn@gmail.com>
2019-09-03 23:55:23 -0700

    updated ingest web UI and lambda-gate

--- lambda/ingest-gate/lambda_function.py
+++ lambda/ingest-gate/lambda_function.py
@@ -1,6 +1,7 @@
-import json
+import json, base64
+import urllib.parse
 import boto3
-import sys
+import sys, traceback
 import uuid
 import os
 from fssi_common import *
@@ -8,7 +9,18 @@ from fssi_common import *
 def lambda_handler(event, context):
     try:
         print('EVENT ', str(event))
-        print('CONTEXT ', str(context))
+
+        tags = {}
+        if 'user_meta' in event["queryStringParameters"]:
+            try:
+                userMeta = event["queryStringParameters"]['user_meta']
+                tags = json.loads(base64.b64decode(urllib.parse.unquote(userMeta)))
+            except:
+                type, err, tb = sys.exc_info()
+                print('caught exception:', err)
+                traceback.print_exc(file=sys.stdout)
+
+        print('user meta tags', tags)
 
         requestFileName = event["queryStringParameters"]['name']
         fileName, fileExtension = os.path.splitext(requestFileName)
@@ -31,7 +43,9 @@ def lambda_handler(event, context):
         }
 
     except:
-        print('caught exception:', sys.exc_info()[0])
+        type, err, tb = sys.exc_info()
+        print('caught exception:', err)
+        traceback.print_exc(file=sys.stdout)
 
     return {
         'statusCode': 404,
